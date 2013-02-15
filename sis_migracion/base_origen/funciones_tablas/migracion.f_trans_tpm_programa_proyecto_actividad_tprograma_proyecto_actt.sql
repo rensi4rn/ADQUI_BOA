9d577bc0ccdf605b2467359295b2b3e755fa5785
gonzaloSarmientoSejas <gonzalo@kplian.com>
2013-02-15 17:10:58 -0400

    -#42 Migracion de la interface persona,usuario, funcionario,institucion Duracion: 16 horas Descripcion : Migracion de las tablas persona, usuario, funcionario, institucion

--- sis_migracion/base_origen/funciones_tablas/migracion.f_trans_tpm_programa_proyecto_actividad_tprograma_proyecto_actt.sql
+++ sis_migracion/base_origen/funciones_tablas/migracion.f_trans_tpm_programa_proyecto_actividad_tprograma_proyecto_actt.sql
@@ -1,17 +1,12 @@
---------------- SQL ---------------
-
-CREATE OR REPLACE FUNCTION migracion.f_trans_tpm_financiador_tfinanciador (
+﻿CREATE OR REPLACE FUNCTION migracion.f_trans_tpm_programa_proyecto_actividad_tprograma_proyecto_actt (
   v_operacion varchar,
-  p_codigo_financiador varchar,
-  p_id_financiador integer,
+  p_id_prog_proy_acti integer,
+  p_id_actividad integer,
+  p_id_programa integer,
+  p_id_proyecto integer,
   p_id_usuario integer,
-  p_descripcion_financiador text,
-  p_fecha_registro date,
-  p_fecha_ultima_modificacion date,
-  p_hora_registro time,
-  p_hora_ultima_modificacion time,
-  p_id_financiador_actif integer,
-  p_nombre_financiador varchar
+  p_ep_actif varchar,
+  p_fecha_reg timestamp
 )
 RETURNS varchar [] AS
 $body$
@@ -27,16 +22,15 @@ DECLARE
 			v_respuesta varchar[];
 			
 			g_registros_resp record;
-			v_codigo_financiador varchar;
-			v_id_financiador int4;
-			v_descripcion_financiador text;
+			v_id_prog_pory_acti int4;
 			v_estado_reg varchar;
 			v_fecha_mod timestamp;
 			v_fecha_reg timestamp;
-			v_id_financiador_actif int4;
+			v_id_actividad int4;
+			v_id_programa int4;
+			v_id_proyecto int4;
 			v_id_usuario_mod int4;
 			v_id_usuario_reg int4;
-			v_nombre_financiador varchar;
 BEGIN
 			
 			
@@ -48,24 +42,26 @@ BEGIN
 			           --previamente se tranforman los datos  (descomentar)
 			           ---------------------------------------
 
-			           
-			v_codigo_financiador= convert(p_codigo_financiador::varchar, 'LATIN1', 'UTF8');
-			v_id_financiador=p_id_financiador::int4;
-			v_descripcion_financiador=convert(p_descripcion_financiador::text, 'LATIN1', 'UTF8');
-			v_estado_reg='activo'::varchar;
-			v_fecha_mod=p_fecha_ultima_modificacion::timestamp;
-			v_fecha_reg=p_fecha_registro::timestamp;
-			v_id_financiador_actif=p_id_financiador_actif::int4;
+
+			v_id_prog_pory_acti=p_id_prog_proy_acti::int4;
+			v_estado_reg=convert('activo'::varchar, 'LATIN1', 'UTF8');
+			v_fecha_mod=NULL::timestamp;
+			v_fecha_reg=p_fecha_reg::timestamp;
+			v_id_actividad=p_id_actividad::int4;
+			v_id_programa=p_id_programa::int4;
+			v_id_proyecto=p_id_proyecto::int4;
 			v_id_usuario_mod=NULL::int4;
-			v_id_usuario_reg=p_id_usuario::int4;
-			v_nombre_financiador=convert(p_nombre_financiador::varchar, 'LATIN1', 'UTF8');
-   
+            IF(p_id_usuario IS NULL)THEN 			
+                v_id_usuario_reg=1::int4;
+            ELSE 
+                v_id_usuario_reg=p_id_usuario::int4;
+            END IF;
+			
 			    --cadena para la llamada a la funcion de insercion en la base de datos destino
 			      
-			   
-                
-			          v_consulta = 'select migra.f__on_trig_tpm_financiador_tfinanciador (
-			               '''||v_operacion::varchar||''','||COALESCE(''''||v_codigo_financiador::varchar||'''','NULL')||','||COALESCE(v_id_financiador::varchar,'NULL')||','||COALESCE(''''||v_descripcion_financiador::varchar||'''','NULL')||','||COALESCE(''''||v_estado_reg::varchar||'''','NULL')||','||COALESCE(''''||v_fecha_mod::varchar||'''','NULL')||','||COALESCE(''''||v_fecha_reg::varchar||'''','NULL')||','||COALESCE(v_id_financiador_actif::varchar,'NULL')||','||COALESCE(v_id_usuario_mod::varchar,'NULL')||','||COALESCE(v_id_usuario_reg::varchar,'NULL')||','||COALESCE(''''||v_nombre_financiador::varchar||'''','NULL')||')';
+			        
+			          v_consulta = 'select migra.f__on_trig_tpm_programa_proyecto_actividad_tprograma_proyecto_acttividad (
+			               '''||v_operacion::varchar||''','||COALESCE(v_id_prog_pory_acti::varchar,'NULL')||','||COALESCE(''''||v_estado_reg::varchar||'''','NULL')||','||COALESCE(''''||v_fecha_mod::varchar||'''','NULL')||','||COALESCE(''''||v_fecha_reg::varchar||'''','NULL')||','||COALESCE(v_id_actividad::varchar,'NULL')||','||COALESCE(v_id_programa::varchar,'NULL')||','||COALESCE(v_id_proyecto::varchar,'NULL')||','||COALESCE(v_id_usuario_mod::varchar,'NULL')||','||COALESCE(v_id_usuario_reg::varchar,'NULL')||')';
 			          --probar la conexion con dblink
 			          
 					   --probar la conexion con dblink
@@ -78,9 +74,8 @@ BEGIN
 			                 
 			             ELSE
 					  
-			               raise notice 'XXXXX %',p_id_financiador;
+			         
 			               PERFORM * FROM dblink(v_consulta,true) AS ( xx varchar);
-                            raise notice 'YYYYY %',p_id_financiador;
 			                v_res_cone=(select dblink_disconnect());
 			             END IF;
 			            
@@ -93,7 +88,6 @@ BEGIN
                  v_respuesta[2]=SQLERRM;
                  v_respuesta[3]=SQLSTATE;
                  v_respuesta[4]=v_consulta;
-              
                  
     
                  
