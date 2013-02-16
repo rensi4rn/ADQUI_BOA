49a3170334a0c7ac0cb748e7d8220aeb62253812
Enzo Rojas <enzorojas@gmail.com>
2013-02-15 21:11:52 -0400

    -Iniciar nuevo sistema de WF (#1), correccion de diagrama ER (referencia, nombres de atributos y longitudes, adicion/eliminacion de algunos atributos), creacion de la estructura dentro del repositorio de pxp y script de creacion de sistema en BD. Time: 4.5
    (Este repositorio seráolo para administrar las tareas asignadas)

--- sis_adquisiciones/base/dependencies000001.sql
+++ sis_adquisiciones/base/dependencies000001.sql
@@ -12,3 +12,18 @@ ALTER TABLE adq.tplan_pago ADD CONSTRAINT fk_tpllan_pago_id_pla_pago_dev FOREIGN
     
 
 /***********************************F-DEP-RAC-ADQ-0-04/01/2013*****************************************/
+
+
+/***********************************I-DEP-FRH-ADQ-0-15/02/2013*****************************************/
+
+
+ALTER TABLE adq.tsolicitud ADD CONSTRAINT fk_solicitud__id_funcionario FOREIGN KEY (id_funcionario) REFERENCES orga.tfuncionario (id_funcionario);
+ALTER TABLE adq.tsolicitud ADD CONSTRAINT fk_solicitud__id_funcionario_solicitud FOREIGN KEY (id_funcionario_aprobador) REFERENCES orga.tfuncionario (id_funcionario);
+ALTER TABLE adq.tsolicitud ADD CONSTRAINT fk_solicitud__id_depto FOREIGN KEY (id_depto) REFERENCES param.tdepto (id_depto);
+ALTER TABLE adq.tsolicitud ADD CONSTRAINT fk_solicitud__id_gestion FOREIGN KEY (id_gestion) REFERENCES param.tgestion (id_gestion);
+ALTER TABLE adq.tsolicitud ADD CONSTRAINT fk_solicitud__id_categoria_compra FOREIGN KEY (id_categoria_compra) REFERENCES adq.tcategoria_compra (id_categoria_compra);
+ALTER TABLE adq.tsolicitud ADD CONSTRAINT fk_solicitud__id_moneda FOREIGN KEY (id_moneda) REFERENCES param.tmoneda (id_moneda);
+ALTER TABLE adq.tsolicitud ADD CONSTRAINT fk_solicitud__id_estado FOREIGN KEY (id_estado) REFERENCES wf.testado (id_estado);
+
+
+/***********************************F-DEP-FRH-ADQ-0-15/02/2013*****************************************/
