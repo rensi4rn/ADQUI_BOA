6a2792bac860dbc21c3122fc0a3985f1d574e3b1
gonzaloSarmientoSejas <gonzalo@kplian.com>
2013-03-06 15:22:27 -0400

    -#65 Titulo: ajuste migracion concepto_ingas,concepto_partida,concepto_cta Duracion: 4 horas Descripcion: Debido a cambios en disenho fue necesario ajustar la migracion de estas tablas

--- sis_migracion/base/funciones/migra.f__on_trig_tpr_concepto_ingas_tconcepto_ingas.sql
+++ sis_migracion/base/funciones/migra.f__on_trig_tpr_concepto_ingas_tconcepto_ingas.sql
@@ -11,7 +11,8 @@ CREATE OR REPLACE FUNCTION migra.f__on_trig_tpr_concepto_ingas_tconcepto_ingas (
   p_id_usuario_mod integer,
   p_id_usuario_reg integer,
   p_sw_tesoro integer,
-  p_tipo varchar
+  p_tipo varchar,
+  p_id_partida integer
 )
 RETURNS text AS
 $body$
@@ -29,18 +30,16 @@ $body$
 						    if(v_operacion = 'INSERT') THEN
 						
 						          INSERT INTO 
-						            PRE.tconcepto_ingas (
+						            param.tconcepto_ingas (
 						id_concepto_ingas,
 						desc_ingas,
 						estado_reg,
 						fecha_mod,
 						fecha_reg,
-						id_item,
 						id_oec,
-						id_servicio,
 						id_usuario_mod,
 						id_usuario_reg,
-						sw_tesoro,
+						sw_tes,
 						tipo)
 				VALUES (
 						p_id_concepto_ingas,
@@ -48,36 +47,52 @@ $body$
 						p_estado_reg,
 						p_fecha_mod,
 						p_fecha_reg,
-						p_id_item,
 						p_id_oec,
-						p_id_servicio,
 						p_id_usuario_mod,
 						p_id_usuario_reg,
 						p_sw_tesoro,
 						p_tipo);
-
+				--insercion a la tabla tconcepto_partida                        
+                INSERT INTO pre.tconcepto_partida(
+                        id_concepto_partida,
+                        id_concepto_ingas,                        
+                        id_partida,
+                        fecha_reg,
+                        id_usuario_reg)
+                VALUES(
+                        p_id_concepto_ingas,
+                        p_id_concepto_ingas,
+                        p_id_partida,
+                        p_fecha_reg,
+                        p_id_usuario_reg);
 						       
 							    ELSEIF  v_operacion = 'UPDATE' THEN
 						               UPDATE 
-						                  PRE.tconcepto_ingas  
+						                  param.tconcepto_ingas  
 						                SET						 desc_ingas=p_desc_ingas
 						 ,estado_reg=p_estado_reg
 						 ,fecha_mod=p_fecha_mod
 						 ,fecha_reg=p_fecha_reg
-						 ,id_item=p_id_item
 						 ,id_oec=p_id_oec
-						 ,id_servicio=p_id_servicio
 						 ,id_usuario_mod=p_id_usuario_mod
 						 ,id_usuario_reg=p_id_usuario_reg
-						 ,sw_tesoro=p_sw_tesoro
+						 ,sw_tes=p_sw_tesoro
 						 ,tipo=p_tipo
 						 WHERE id_concepto_ingas=p_id_concepto_ingas;
-
-						       
-						       ELSEIF  v_operacion = 'DELETE' THEN
+				--actualizacion de la tabla tconcepto_partida
+				UPDATE pre.tconcepto_partida SET
+                	id_concepto_ingas=p_id_concepto_ingas,
+                    id_partida=p_id_partida,
+                    fecha_mod=p_fecha_mod,
+					fecha_reg=p_fecha_reg,
+					id_usuario_mod=p_id_usuario_mod,
+					id_usuario_reg=p_id_usuario_reg
+				WHERE id_concepto_partida=p_id_concepto_ingas;
+					
+                	       ELSEIF  v_operacion = 'DELETE' THEN
 						       
 						         DELETE FROM 
-						              PRE.tconcepto_ingas
+						              param.tconcepto_ingas
  
 						              						 WHERE id_concepto_ingas=p_id_concepto_ingas;
 
