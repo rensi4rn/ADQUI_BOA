d72815dee66e2791067d7fca03ebd269b13c5c7f
gonzaloSarmientoSejas <gonzalo@kplian.com>
2013-03-13 17:38:34 -0400

    arreglado archivo data.sql para restaurar base de datos

--- sis_adquisiciones/base/test_data.sql
+++ sis_adquisiciones/base/test_data.sql
@@ -13,13 +13,5 @@ VALUES (1, NULL, E'2013-03-04 20:38:26', E'2013-03-04 20:38:26', E'activo', 105,
 /* Data for the 'param.tdocumento' table  (Records 1 - 1) */
 
 INSERT INTO param.tdocumento ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_documento", "id_subsistema", "codigo", "descripcion", "periodo_gestion", "tipo", "tipo_numeracion", "formato")
-VALUES (1, NULL, E'2013-03-04 00:00:00', E'2013-03-04 20:32:58.442', E'activo', 1, 6, E'SOLB', E'Solicitud de bien', E'gestion', E'', E'depto', NULL);
+VALUES (1, NULL, E'2013-03-04 00:00:00', E'2013-03-04 20:32:58.442', E'activo', 2, 6, E'SOLB', E'Solicitud de bien', E'gestion', E'', E'depto', NULL);
 
-
-/* Data for the 'adq.tcategoria_compra' table  (Records 1 - 2) */
-
-INSERT INTO adq.tcategoria_compra ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_categoria_compra", "codigo", "nombre", "min", "max", "obs")
-VALUES (1, NULL, E'2013-03-04 20:34:55.327', NULL, E'activo', 2, E'COLO', E'Compra Local', '1', '199999', E'');
-
-INSERT INTO adq.tcategoria_compra ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_categoria_compra", "codigo", "nombre", "min", "max", "obs")
-VALUES (1, 1, E'2013-03-04 20:34:36.749', E'2013-03-04 20:35:12.846', E'activo', 1, E'COIN', E'Compra Internacional', '200000', '1000000', E'');
